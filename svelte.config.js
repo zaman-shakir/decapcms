6959c1a84f6d0b92dd90cde150c3db80641be620
Shakir Uz Zaman <zaman.shakirdev@gmail.com>
2024-02-06 11:21:34 +0600

    items collection added for json format testing

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,20 @@
 // svelte.config.js
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
+import { mdsvex } from 'mdsvex';
+
 //import adapter from '@sveltejs/adapter-node';
 
 export default {
+    extensions: ['.svelte', '.svx'], // Add .svx extension for MDsveX files
+    preprocess: [
+      mdsvex(), // Enable MDsveX preprocessing
+      preprocess({
+        scss: {
+          prependData: `@import './src/global.scss';`, // Optional: Import global styles
+        },
+      }),
+    ],
   kit: {
     adapter: adapter(),
     // Other kit configurations...
